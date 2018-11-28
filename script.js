b10363620a38dc50bb0cb217a98786f952c53dd4
azzaan <azza-a-n@hotmail.com>
2018-11-28 18:04:26 +0300

    finished 0.1

--- script.js
+++ script.js
@@ -11,6 +11,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
+const NS = 'org.example.empty';
+
+
+/**
+ * create an order to purchase
+ * @param {org.example.empty.RecAccessToHospital} purchase - the order to be processed
+ * @transaction
+ */
+async function RecAccessToHospital(data) {
+    data.vaccineRecord.hospital = data.hospital; 
+    const assetRegistry = await getAssetRegistry(NS+'.VaccineRecord');
+  
+    await assetRegistry.update(data.vaccineRecord);
+  }
+
+/**
+ * create an order to purchase
+ * @param {org.example.empty.RecAccessToInsurance} purchase - the order to be processed
+ * @transaction
+ */
+async function RecAccessToInsurance(data) {
+    data.vaccineRecord.insurance = data.insurance; 
+    const assetRegistry = await getAssetRegistry(NS+'.VaccineRecord');
+  
+    await assetRegistry.update(data.vaccineRecord);
+  }
+
+/**
+ * create an order to purchase
+ * @param {org.example.empty.RecAccessToSchool} purchase - the order to be processed
+ * @transaction
+ */
+async function RecAccessToSchool(data) {
+    data.vaccineRecord.school = data.school; 
+    const assetRegistry = await getAssetRegistry(NS+'.VaccineRecord');
+  
+    await assetRegistry.update(data.vaccineRecord);
+  }
 
 /**
  * create an order to purchase
@@ -19,36 +58,67 @@
  */
 async function DocNote(data) {
   data.detail.note = data.note; 
-  let assetRegistry = await getAssetRegistry('org.example.empty.VaccineDetail');
-
-  await assetRegistry.update(data.detail);
-}
+    const assetRegistry = await getAssetRegistry(NS+'.VaccineDetail');
+  
+    await assetRegistry.update(data.detail);
+  }
 
 /**
  * create an order to purchase
- * @param {org.example.empty.CreateVaccineDetail} purchase - the order to be processed
+ * @param {org.example.empty.PhysicianSignature} purchase - the order to be processed
  * @transaction
  */
-async function CreateVaccineDetail(data) {
-  const factory = getFactory();
-  const NS = 'org.example.empty';
-  let vacDetailId = new Date().valueOf()+"";
-  const vaccineDetail = factory.newResource(NS, 'VaccineDetail', vacDetailId);
+async function PhysicianSignature(data) {
+    data.detail.signed = data.signed; 
+    const assetRegistry = await getAssetRegistry(NS+'.VaccineDetail');
   
-  vaccineDetail.vaccineName = data.vaccineName;
-  vaccineDetail.physician = data.physician;
-  vaccineDetail.doc = data.doc;
-  vaccineDetail.vacRec = data.vacRec;
-  
-  const contractRegistry = await getAssetRegistry(NS + '.VaccineDetail');
-  try{
-    let added = await contractRegistry.addAll([vaccineDetail]);
-    debugger;
-    vaccineDetail.vacRec.vaccineDetails.push(factory.newRelationship(NS, 'VaccineDetail', vacDetailId));
-    let assetRegistry = await getAssetRegistry('org.example.empty.VaccineRecord');
-    await assetRegistry.update(vaccineDetail.vacRec);
-  }catch(e){
-    console.log(e);
+    await assetRegistry.update(data.detail);
   }
   
-}
+
+/**
+   * create an order to purchase
+   * @param {org.example.empty.CreateVaccineRecord} purchase - the order to be processed
+   * @transaction
+   */
+  async function CreateVaccineRecord(data) {
+    const factory = getFactory();
+    const id = new Date().valueOf()+"";
+    const vaccineRecord = factory.newResource(NS, 'VaccineRecord', id);
+    
+    vaccineRecord.family = data.family;
+    
+    const contractRegistry = await getAssetRegistry(NS + '.VaccineRecord');
+    
+    await contractRegistry.addAll([vaccineRecord]);
+     
+  }
+  /**
+   * create an order to purchase
+   * @param {org.example.empty.CreateVaccineDetail} purchase - the order to be processed
+   * @transaction
+   */
+  async function CreateVaccineDetail(data) {
+    const factory = getFactory();
+    const id = new Date().valueOf()+"";
+    const vaccineDetail = factory.newResource(NS, 'VaccineDetail', id);
+    
+    vaccineDetail.vaccineName = data.vaccineName;
+    vaccineDetail.physician = data.physician;
+    vaccineDetail.doc = data.doc;
+    vaccineDetail.vacRec = data.vacRec;
+    
+    const contractRegistry = await getAssetRegistry(NS + '.VaccineDetail');
+    try{
+      const added = await contractRegistry.addAll([vaccineDetail]);
+      debugger;
+      if(!vaccineDetail.vacRec.vaccineDetails) {
+    		vaccineDetail.vacRec.vaccineDetails = [];
+  		}
+      vaccineDetail.vacRec.vaccineDetails.push(factory.newRelationship(NS, 'VaccineDetail', id));
+      const assetRegistry = await getAssetRegistry(NS+'.VaccineRecord');
+      await assetRegistry.update(vaccineDetail.vacRec);
+    }catch(e){
+    	console.log(e);
+    }
+  }
